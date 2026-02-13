1c04c73701ebd60405f3f861e8d6d3088ef0121f
Your Name <you@example.com>
2026-02-13 22:15:22 +0800

    更新試算表 CSV 分頁連結

--- src/data/config.js
+++ src/data/config.js
@@ -1,2 +1,2 @@
 // 修正後的正確發布連結 (修正了 ZIL2c2 的字母錯誤)
-export const PUBLISHED_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSgCHUOw6ZyYlZwsLduNBJjtWMpmGWn2jdx1ZIL2c2KYPGuhzF3f6R6mZi_QFrsvQ/pub?output=csv";
+export const PUBLISHED_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSgCHUow6ZyYlZwsLduNBJjtwMpmGWn2jdx1ZlL2c2KYPGuhzF3f6R6mZi_QFrsvQ/pub?gid=1649002029&single=true&output=csv";
